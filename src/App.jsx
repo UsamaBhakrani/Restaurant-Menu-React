130f7a9fc9f02e80ab86abe7d103a63b7251ca35
Mohammed Usama <usamabhakrani@gmail.com>
2023-07-24 17:58:34 +0500

    Images and Structure of App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import {menu} from "./data";
+import Categories from "./Categories";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <section className="menu">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="title-underline"></div>
+        </div>
+        <div className="btn-container">{menu.map(item=>{
+          return <button type="button" className="btn">{}</button>
+        })}</div>
+        <div className="section-center"></div>
+      </section>
+    </main>
+  );
 };
 
 export default App;
